f614785b3fa01757fbf15b220f3860a2203ec8c2
saretoduncan <saretoduncan@gmail.com>
2021-09-24 19:13:17 +0300

    add change water function

--- src/main/kotlin/AMS.kt
+++ src/main/kotlin/AMS.kt
@@ -9,14 +9,16 @@ fun feedFish() {
     val food = fishFood(day)
     println("Today is $day and the fish eat $food")
 }
-
+fun shouldChangeWater(temperature:Int=22, dirty:Int=20, day:String):Boolean {//change water function
+    return true
+}
 fun randomDay(): String {
-    val daysOfTheWeek= listOf("Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+    val daysOfTheWeek= listOf("Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")//daysOfTheWeek
     return daysOfTheWeek[Random().nextInt(7)]
 
 }
 
-fun fishFood(day:String): String{
+fun fishFood(day:String): String{//fish food function
     return when (day){//switch statement
         "Monday" -> "flakes"
         "Tuesday" -> "pellets"
